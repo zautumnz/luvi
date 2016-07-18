2a7af88be35aee877659893fef7b293ac40885bf
zacanger <zac@zacanger.com>
2016-07-17 21:28:45 -0600

    version bump

--- src/help.js
+++ src/help.js
@@ -2,12 +2,12 @@ const help = `
 ♡ luvi (a server)
 ------------------
 usage:
-    ♡ luvi                   # launch default server
-    ♡ luvi foo bar           # start servers \`foo\` & \`bar\`
-    ♡ luvi -p 1337           # listen on specified port
-    ♡ luvi -r /path/to/root  # serve from specified dir
-    ♡ luvi -v                # luvi version
-    ♡ luvi -h                # this help
+    ♡ luvi           # launch default server
+    ♡ luvi foo bar   # start servers 'foo' & 'bar'
+    ♡ luvi -p 1337   # listen on specified port
+    ♡ luvi -r /path  # serve from specified dir
+    ♡ luvi -v        # luvi version
+    ♡ luvi -h        # this help
                              --------------------
 see the readme for config options and api usage
 `
