821c0c423ab641e0abc66c8eb621ddd56f3454bf
zacanger <zac@zacanger.com>
2016-07-18 20:08:11 -0600

    color - blue test description fix

--- src/lib/test.js
+++ src/lib/test.js
@@ -72,7 +72,7 @@ test('mix mixes two objects', t => {
 })
 
 // color
-test('color.blue()', t => {
+test('color.blue() returns string wrapped in appropriate escape codes', t => {
   const col = util.inspect(color.blue('foo'))
   const str = '\'\\u001b[34mfoo\\u001b[39m\''
   t.is(col, str)
