764892400cea46292a7cbfc099b95dca70fc5399
zacanger <zac@zacanger.com>
2016-07-30 10:34:05 -0600

    version bump

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 const
   minimist    = require('minimist')
 , luvi        = require('./luvi')
+, help        = require('./help')
 , mix         = require('./lib/mix')
 , each        = require('./lib/each')
 , clrs        = require('./lib/color')
@@ -14,10 +15,9 @@ const
 , isArrayLike = require('./lib/isArrayLike')
 , pkg         = readJSON(__dirname, 'package.json')
 , argv        = minimist(process.argv.slice(2))
-, log         = console.log
+, l           = console.log
 , configFile  = argv.config || `.${pkg.name}.json`
 , config      = readJSON(configFile)
-, help        = require('./help')
 , version     = `❤ luvi ${pkg.version}`
 let servers   = isArrayLike(config) ? config : [config]
 
@@ -25,13 +25,13 @@ if (argv.v)       {argv.version = argv.v}
 if (argv.h)       {argv.help    = argv.h}
 if (argv.r)       {argv.root    = argv.r}
 if (argv.p)       {argv.port    = argv.p}
-if (argv.version) {return log(clrs.yellow(version))}
-if (argv.help)    {return log(clrs.cyan(help))}
+if (argv.version) {return l(clrs.yellow(version))}
+if (argv.help)    {return l(clrs.cyan(help))}
 
 if (argv._.length) {
-  servers = filter(servers, item => {
-    return item && argv._.indexOf(item.name) >= 0
-  })
+  servers = filter(servers, item =>
+    item && argv._.indexOf(item.name) >= 0
+  )
 }
 
 each(servers, server => {
